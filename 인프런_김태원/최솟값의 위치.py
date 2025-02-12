ea7b0e2a203524cd9135eaeb42b4185ebffdd451
서성진 <tjtjdwls0614@gmail.com>
2025-02-12 15:08:46 +0900

    인프런 강의 시작

---
+++ 인프런_김태원/최솟값의 위치.py
@@ -0,0 +1,19 @@
+# 제한사항:
+# 입력값 범위: 3 <= n <= 100,000
+# 시간복잡도: O(n), O(nlogn)을 넘지 않는다.
+
+# 입력값으로 배열을 입력받음
+# 배열의 원소를 순차적으로 순회함
+# 기본값을 설정하고 기본값보다 작다면 값을 변경함
+
+data = input()
+
+min = 0
+answer = 0
+
+for i in data:
+  if data[i] < min:
+    min = data[i]
+    answer = i
+    
+print(answer)
